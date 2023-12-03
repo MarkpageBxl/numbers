eee173ba681cd1e2c859e6ad4b20f48ad6ae0c8d
Markus Lindström <markus.lindstrom@markpage.be>
2023-12-03 12:17:33 +0100

    Rename and fixup.

--- numbergame.py
+++ numbergame.py
@@ -1,49 +1,70 @@
 #!/usr/bin/env python3
 
+import argparse
 import colorsys
-import pygame
 import random
 import sys
 
+import pygame
 from espeakng import ESpeakNG
 
-def next_number():
-   return random.randint(0, 9999)
+
+def number_gen(m: int, n: int) -> int:
+    nums = list(range(m, n + 1))
+    # random.shuffle(nums)
+    for i in nums:
+        yield i
+
 
 def next_color():
-    return [int(x*255) for x in colorsys.hls_to_rgb(random.random(), 0.6, 1)]
+    return [int(x * 255) for x in colorsys.hls_to_rgb(random.random(), 0.6, 1)]
+
 
 def speak(n):
     speech.say(str(n), sync=True)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--min", "-m", type=int, default=0)
+parser.add_argument("--max", "-n", type=int, default=9999)
+args = parser.parse_args()
 
 pygame.init()
 size = width, height = 720, 360
 black = 0, 0, 0
 white = 255, 255, 255
 speech = ESpeakNG()
-speech.voice = 'fr-be'
+speech.voice = "fr-be"
 speech.speed = 120
 speech.pitch = 75
 
 screen = pygame.display.set_mode(size)
 pygame.display.toggle_fullscreen()
 pygame.display.set_caption("Numbers game")
-font = pygame.font.SysFont('liberationsans', 256, True, False)
-n = next_number()
+font = pygame.font.SysFont("liberationsans", 256, True, False)
+gen = number_gen(args.min, args.max)
+n = next(gen)
 color = next_color()
 
-while True:
+done = False
+while not done:
     played = False
     for event in pygame.event.get():
         if played:
             continue
-        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+        if event.type == pygame.QUIT or (
+            event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE
+        ):
             pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
             speak(n)
             color = next_color()
-            n = next_number()
+            try:
+                n = next(gen)
+            except StopIteration:
+                done = True
+                break
             played = True
         elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
             speak(n)
